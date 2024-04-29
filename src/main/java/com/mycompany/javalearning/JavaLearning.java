c8e6ae5d5f2557be4af68beb57e11980ef41cc56
ohamjoseph <ouilyh@gmail.com>
2024-04-29 12:58:49 +0000

    Ajout de la methode de calcul de la superfie d'un rectangle

--- src/main/java/com/mycompany/javalearning/JavaLearning.java
+++ src/main/java/com/mycompany/javalearning/JavaLearning.java
@@ -12,5 +12,16 @@ public class JavaLearning {
 
     public static void main(String[] args) {
         System.out.println("I'l Learning Java");
+        
+    }
+    
+    /**
+     * Pour calculer la superficie d'un rectangle
+     * @param longueur float longueur du rectangle
+     * @param largeur float largeur du rectangle
+     * @return float superfie d'un rectangle
+     */
+    public float calculerSuperficieRectangle(final float longueur, final float largeur){
+        return longueur*largeur;
     }
 }
